34a9a1a3053918a1b030102880e03a761d421bad
ding.tao <ding.tao@yff.com>
2021-12-09 09:14:05 +0800

    init

--- LeetCode/String.playground/Contents.swift
+++ LeetCode/String.playground/Contents.swift
@@ -3,3 +3,9 @@ import Foundation
 // 验证回文串
 
 
+// LRU
+
+class Node {
+    var data: Int = 0
+    var next: Node?
+}
---
+++ SwiftUI/MyPlayground.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+var greeting = "Hello, playground"
+let arr = "0".components(separatedBy: ".")
+
+print(arr)
